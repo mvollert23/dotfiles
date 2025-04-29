ee66819fb1401fbd17e67c9ab85abff8c6540f7f
Michael Vollert <git@mvollert.de>
2025-04-30 00:25:43 +0200

    nvim: copilot use <C-e> to accept, rather than <tab>

--- .config/nvim/lua/mvo/lazy/copilot.lua
+++ .config/nvim/lua/mvo/lazy/copilot.lua
@@ -3,9 +3,13 @@ return {
   {
     "github/copilot.vim",
     lazy = false,
-    config = function() -- Mapping tab is already used in NvChad
-      -- vim.g.copilot_no_tab_map = true; -- Disable tab mapping
-      -- vim.g.copilot_assume_mapped = true; -- Assume that the mapping is already done
+    config = function() -- Mapping tab is cumbersome
+        vim.g.copilot_no_tab_map = true; -- Disable tab mapping
+        vim.g.copilot_assume_mapped = true; -- Assume that the mapping is already done
+        vim.keymap.set('i', '<C-E>', 'copilot#Accept("\\<CR>")', {
+          expr = true,
+          replace_keycodes = false
+        })
     end
   }
 }
